a71dbe143cce53afa577c1db99d466aa9060db14
mariuslazar93 <mariuslazar93@yahoo.ro>
2017-01-08 11:33:57 +0200

    fixed bundles and finish UI workflow

--- Under33.Solution/Under33.Web/App_Start/BundleConfig.cs
+++ Under33.Solution/Under33.Web/App_Start/BundleConfig.cs
@@ -8,21 +8,15 @@ namespace WebApplication1
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/vendor/jquery/jquery-3.1.1.min.js",
+                        "~/Scripts/vendor/bootstrap/bootstrap.min.js",
+                        "~/Scripts/app/main.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Content/css/global.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
